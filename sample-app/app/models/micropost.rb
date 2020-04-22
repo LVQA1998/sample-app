7321124e2c0a64ec948a17d015ecbc819fe80a70
LVQA1998 <quanganlevan@gmail.com>
2020-04-22 08:23:44 +0700

    chapter 13

---
+++ sample-app/app/models/micropost.rb
@@ -0,0 +1,16 @@
+class Micropost < ApplicationRecord
+  MICROPOST_PARAMS = [:content, :image]
+  belongs_to :user
+  has_one_attached :image
+  
+  scope :by_created_at, -> {order created_at: :desc}
+  scope :feed_user_id, ->(user_id) {where user_id: user_id}
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: Settings.micropost.content_max}
+  validates :image, content_type: {in: Settings.image.extension_whitelist, message: I18n.t("microposts.image_format")}, size: {less_than: Settings.image.size.megabytes, message: I18n.t("microposts.image_size", size: Settings.image.size)}
+  
+  def print_image
+    image.variant resize_to_limit: Settings.image.resize
+  end
+end

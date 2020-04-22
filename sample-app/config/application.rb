fbe1311eb6ac83ee705a9c847fd7b0bcfc93d97f
LVQA1998 <quanganlevan@gmail.com>
2020-04-22 08:23:44 +0700

    init-project

---
+++ sample-app/config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+  end
+end
---
+++ sample-app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  end
+end

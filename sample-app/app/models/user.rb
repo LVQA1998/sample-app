daab5b68d27a2837013518caa1fc65b9b58ede41
LVQA1998 <63584446+LVQA1998@users.noreply.github.com>
2020-04-22 07:59:10 +0700

    chapter 7: Sign up

--- sample-app/app/models/user.rb
+++ sample-app/app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.email_regex
+  USER_PARAMS = [:name, :email, :password, :password_confirmation].freeze
   before_save :downcase_email
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  
   private
+  
   def downcase_email
     email.downcase!
   end

fe8a68535215d19c23d6526080369890625cf5a4
LVQA1998 <quanganlevan@gmail.com>
2020-04-22 08:23:44 +0700

    chapter 10

--- sample-app/app/controllers/users_controller.rb
+++ sample-app/app/controllers/users_controller.rb
@@ -1,14 +1,37 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :admin_user, only: :destroy
+  before_action :load_user, only: %i(edit show update)
+
+  def index
+    @users = User.page(params[:page]).per(Settings.record_per_page)
+  end
+
   def new
     @user = User.new
   end
 
-  def show 
-    @user = User.find_by id: params[:id]
-    return if @user.present? 
-    
-    flash[:danger] = t "undefine_user"
-    redirect_to root_path
+  def edit; end
+
+  def show; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "sessions.flash.update_success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t "sessions.flash.edit_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.index.user_delete"
+    else
+      flash[:danger] = t "sessions.flash.destroy_fail"
+    end
+    redirect_to users_url
   end
 
   def create
@@ -17,12 +40,37 @@ class UsersController < ApplicationController
       flash[:success] = t "greeting" 
       redirect_to @user
     else
-      flash[:danger] = t "signup_fail"
+      flash[:danger] = t "sessions.flash.signup_fail"
       render :new
     end
   end
 
+  private
+
   def user_params
     params.require(:user).permit User::USER_PARAMS
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "sessions.flash.login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+  
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "sessions.flash.undefine_user"
+    redirect_to root_url
   end
 end
--- sample-app/app/models/user.rb
+++ sample-app/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  validates :password, presence: true, length: {minimum: Settings.password.minimum}, allow_nil: true
 
   class << self
     def digest string 
@@ -21,7 +21,6 @@ class User < ApplicationRecord
   end
 
   def remember
-    
     self.remember_token = User.new_token
     update_attribute :remember_digest, User.digest(remember_token)
   end
@@ -35,9 +34,34 @@ class User < ApplicationRecord
     update_attribute :remember_digest, nil
   end
   
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+  
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end

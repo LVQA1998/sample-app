78c35997ed4b0d336182c7fce5d29885b6ef5631
LVQA1998 <quanganlevan@gmail.com>
2020-04-22 08:23:44 +0700

    chapter 10

--- sample-app/app/controllers/sessions_controller.rb
+++ sample-app/app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
   def new; end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
-      remember user
-      redirect_to user
+      params[:session][:remember_me] == Settings.remember_me ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash[:danger] = t "sessions.flash.danger"
       render :new
     end
-  end
-  
-  def destroy
-    log_out
-    redirect_to root_url
   end
 end

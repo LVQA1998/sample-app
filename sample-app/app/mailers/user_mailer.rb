fd4410db78b704cc9de883d17edfd9626c4c9e57
LVQA1998 <quanganlevan@gmail.com>
2020-04-22 08:23:44 +0700

    chapter 12

--- sample-app/app/mailers/user_mailer.rb
+++ sample-app/app/mailers/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t("mailer.account_activation")
   end
 
-  def password_reset
-    mail to: Settings.reset_password_email
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_resets.new.password_reset")
   end
 end
